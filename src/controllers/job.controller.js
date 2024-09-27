18ac7a33be3d9e344e0c743a0f15bb5b70aebaa9
GopalGavas <gopalgavas25@outlook.com>
2024-09-27 16:39:02 +0530

    add sorting , filtering and pagination

--- src/controllers/job.controller.js
+++ src/controllers/job.controller.js
@@ -2,7 +2,9 @@ import { asyncHandler } from "../utils/asyncHandler.js";
 import { ApiError } from "../utils/apiError.js";
 import { ApiResponse } from "../utils/apiResponse.js";
 import { Job } from "../models/jobs.model.js";
-import mongoose from "mongoose";
+import mongoose, { isValidObjectId } from "mongoose";
+import moment from "moment";
+import { application, query } from "express";
 
 const createJob = asyncHandler(async (req, res) => {
   const { company, position, workLocation } = req.body;
@@ -31,18 +33,69 @@ const createJob = asyncHandler(async (req, res) => {
 });
 
 const getAllJobs = asyncHandler(async (req, res) => {
-  const jobs = await Job.find({ createdBy: req.user?._id });
+  // const jobs = await Job.find({ createdBy: req.user?._id });
 
-  if (!jobs) {
-    throw new ApiError(404, "Can't find all the job  postings");
+  const { applicationStatus, workType, search, sort } = req.query;
+
+  const queryObject = {};
+
+  if (applicationStatus && applicationStatus !== "all") {
+    queryObject.applicationStatus = applicationStatus;
   }
+
+  if (workType && workType !== "all") {
+    queryObject.workType = workType;
+  }
+
+  if (search) {
+    queryObject.position = { $regex: search, $options: "i" };
+  }
+
+  const resultQuery = [
+    {
+      $match: queryObject,
+    },
+    {
+      $addFields: {
+        positionLower: { $toLower: "$position" },
+      },
+    },
+    {
+      $sort: {
+        ...(sort === "latest" && { createdAt: -1 }),
+        ...(sort === "oldest" && { createdAt: 1 }),
+        ...(sort === "a-z" && { positionLower: 1 }),
+        ...(sort === "z-a" && { positionLower: -1 }),
+        ...(sort !== "latest" &&
+          sort !== "oldest" &&
+          sort !== "a-z" &&
+          sort !== "z-a" && { createdAt: -1 }), // Default sort if none match
+      },
+    },
+  ];
+
+  // "Pagination"
+  const page = Number(req.query.page) || 1;
+  const limit = Number(req.query.limit) || 10;
+  const skip = (page - 1) * limit;
+
+  resultQuery.push({ $skip: skip });
+  resultQuery.push({ $limit: limit });
+
+  const jobs = await Job.aggregate(resultQuery);
+
+  //jobsCount
+  const totalJobs = await Job.countDocuments(queryObject);
+  const numOfPage = totalJobs > 0 ? Math.ceil(totalJobs / limit) : 0;
 
   return res.status(200).json(
     new ApiResponse(
       200,
       {
-        numberOfPostings: jobs.length,
+        totalNumberOfPostings: totalJobs,
+        numberOfPostingsOnPage: jobs.length,
         jobs,
+        numOfPage,
       },
       "All Postings fetched successfully"
     )
@@ -53,7 +106,7 @@ const updateJobDetails = asyncHandler(async (req, res) => {
   const { jobId } = req.params;
   const { company, position, workLocation } = req.body;
 
-  if (!mongoose.isValidObjectId(jobId)) {
+  if (!isValidObjectId(jobId)) {
     throw new ApiError(400, "Enter a valid JobId");
   }
 
@@ -106,4 +159,107 @@ const updateJobDetails = asyncHandler(async (req, res) => {
     .json(new ApiResponse(200, updatedJob, "Job Posting updated successfully"));
 });
 
-export { createJob, getAllJobs, updateJobDetails };
+const deleteJobPosting = asyncHandler(async (req, res) => {
+  const { jobId } = req.params;
+
+  if (!isValidObjectId(jobId)) {
+    throw new ApiError(400, "Invalid job id");
+  }
+
+  const job = await Job.findById(jobId);
+
+  if (!job) {
+    throw new ApiError(404, "Job not found");
+  }
+
+  if (job.createdBy.toString() !== req.user?._id.toString()) {
+    throw new ApiError(401, "You are not Authorized for this action");
+  }
+
+  const deleteJob = await Job.findByIdAndDelete(jobId);
+
+  if (!deleteJob) {
+    throw new ApiError(
+      500,
+      "Failed to delete Job Posting , please try again letter"
+    );
+  }
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, {}, "Job Posting deleted successfully"));
+});
+
+const jobStats = asyncHandler(async (req, res) => {
+  const stats = await Job.aggregate([
+    {
+      $match: {
+        createdBy: new mongoose.Types.ObjectId(req.user?._id),
+      },
+    },
+    {
+      $group: {
+        _id: "$applicationStatus",
+        count: { $sum: 1 },
+      },
+    },
+  ]);
+
+  let monthlyApplication = await Job.aggregate([
+    {
+      $match: {
+        createdBy: new mongoose.Types.ObjectId(req.user?._id),
+      },
+    },
+
+    {
+      $group: {
+        _id: {
+          year: { $year: "$createdAt" },
+          month: { $month: "$createdAt" },
+        },
+        count: {
+          $sum: 1,
+        },
+      },
+    },
+  ]);
+
+  if (stats.length === 0) {
+    return res
+      .status(200)
+      .json(
+        new ApiResponse(
+          200,
+          { totalJobs: 0, stats: [], monthlyApplication: [] },
+          `No jobs found`
+        )
+      );
+  }
+
+  monthlyApplication = monthlyApplication.map((application) => {
+    const {
+      _id: { month, year },
+      count,
+    } = application;
+
+    const date = moment()
+      .month(month - 1)
+      .year(year)
+      .format("MMM Y");
+
+    return { date, count };
+  });
+
+  return res
+    .status(200)
+    .json(
+      new ApiResponse(
+        200,
+        { totalJobs: stats.length, stats, monthlyApplication },
+        `jobs fetched successfully`
+      )
+    );
+});
+
+export { createJob, getAllJobs, updateJobDetails, deleteJobPosting, jobStats };
--- src/routes/job.routes.js
+++ src/routes/job.routes.js
@@ -2,14 +2,20 @@ import { Router } from "express";
 import { verifyJWT } from "../middleware/auth.middleware.js";
 import {
   createJob,
+  deleteJobPosting,
   getAllJobs,
+  jobStats,
   updateJobDetails,
 } from "../controllers/job.controller.js";
 
 const router = Router();
 
-router.route("/create-job").post(verifyJWT, createJob);
-router.route("/get-job").get(verifyJWT, getAllJobs);
-router.route("/update-job/:jobId").patch(verifyJWT, updateJobDetails);
+router.use(verifyJWT);
+
+router.route("/create-job").post(createJob);
+router.route("/get-job").get(getAllJobs);
+router.route("/update-job/:jobId").patch(updateJobDetails);
+router.route("/delete-job/:jobId").delete(deleteJobPosting);
+router.route("/stats").get(jobStats);
 
 export default router;
